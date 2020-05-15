8672ce6179af7d4ff36b029e6f6f70ff4a358cf0
nadaanbaalak <abhi.sharma2800015@gmail.com>
2020-05-15 17:51:57 +0530

    Finished with styling

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar fixed-top navbar-dark bg-dark">
+      <span className="navbar-brand mb-0 h1">
+        <b>Greendeck</b>
+      </span>
+    </nav>
+  );
+};
+
+export default Navbar;
